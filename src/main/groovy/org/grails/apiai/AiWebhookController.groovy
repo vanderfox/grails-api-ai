38da5738f77b7879dfe8c4f0d984f993a29462d9
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2017-05-27 00:34:41 -0500

    finally working with api.ai

--- src/main/groovy/org/grails/apiai/AiWebhookController.groovy
+++ src/main/groovy/org/grails/apiai/AiWebhookController.groovy
@@ -8,13 +8,17 @@ import grails.web.Action
 import grails.web.api.WebAttributes
 import groovy.util.logging.Slf4j
 
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 /**
  * Created by rvanderwerf on 5/11/17.
  */
 @Slf4j
 trait AiWebhookController implements WebAttributes {
 
-    private static final String RESPONSE_CONTENT_TYPE = "Content-Type: application/json"
+    private static final String RESPONSE_CONTENT_TYPE = "application/json"
 
     private static final String RESPONSE_CHARACTER_ENCODING = "utf-8"
 
@@ -35,13 +39,19 @@ trait AiWebhookController implements WebAttributes {
     def index() {
         Fulfillment output = new Fulfillment()
         String incomingJson = request.JSON.toString()
-        log.debug("incoming json=${incomingJson}")
+        log.debug("incoming json:\n ${incomingJson}")
         doWebhook(gson.fromJson(incomingJson, AIWebhookServlet.AIWebhookRequest.class), output)
         response.setCharacterEncoding(RESPONSE_CHARACTER_ENCODING)
         response.setContentType(RESPONSE_CONTENT_TYPE)
+        response.setHeader('Date', DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("GMT"))))
         response.setHeader('Access-Control-Allow-Origin','*')
 
-        gson.toJson(output, response.getWriter())
+        StringWriter stringWriter = new StringWriter()
+        gson.toJson(output, stringWriter)
+        String webHookOutput = stringWriter.toString()
+        response.setHeader('Content-Length',webHookOutput.size() as String)
+        log.debug("outgoing json:\n ${webHookOutput}")
+        response << webHookOutput
         response.flushBuffer() // these 2 lines force grails to bypass the view layer
         null
     }
